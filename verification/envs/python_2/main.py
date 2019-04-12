b6064ac618354140ed90be1350aa8acb38c0a17d
Alexander Lyabah <a.lyabah@checkio.org>
2019-04-12 14:15:49 +0300

    new python image

--- verification/envs/python_2/main.py
+++
@@ -1,4 +0,0 @@
-import sys
-from checkio_executor_python.client import ClientLoop
-
-ClientLoop(int(sys.argv[1]), sys.argv[2]).start()
--- verification/envs/python_3/run.sh
+++ verification/envs/python_3/run.sh
@@ -17,4 +17,4 @@
 SCRIPT=`realpath $0`
 SCRIPTPATH=`dirname $SCRIPT`
 
-python3 $SCRIPTPATH/main.py $1 $2
+python $SCRIPTPATH/main.py $1 $2
