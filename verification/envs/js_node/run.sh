65f3044e849d85a969f5cd17b5f5890f71a7db35
Alexander Lyabah <a.lyabah@checkio.org>
2019-05-15 18:29:25 +0300

    m

--- verification/envs/js_node/run.sh
+++ verification/envs/js_node/run.sh
@@ -5,5 +5,5 @@ SCRIPTPATH=`dirname $SCRIPT`
 cd $SCRIPTPATH
 export NODE_PATH=/lib/node_modules/:$SCRIPTPATH
 touch userModule.js
-chmow a+rw userModule.js
+chmod a+rw userModule.js
 node --harmony main.js $1 $2
